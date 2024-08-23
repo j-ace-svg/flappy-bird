e706eb9d26f89d391b2f60ff26c9cd09066a5b09
John Montgomery <j.ace.svg@gmail.com>
2024-08-23 18:51:23 +0000

    End of same day

--- main.py
+++ main.py
@@ -24,9 +24,38 @@ ground_img = pygame.image.load('img/ground.png')
 class Bird(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('img/bird1.png')
+        self.images = []
+        self.index = 0
+        self.counter = 0
+        for num in range(1, 4):
+            img = pygame.image.load(f'img/bird{num}.png')
+            self.images.append(img)
+        self.image = self.images[self.index]
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
+        self.vel = 0
+
+    def update(self):
+
+        #gravity
+        #self.vel += 0.5
+        if self.vel > 8:
+            self.vel = 8
+        if self.rect.bottom < 768:
+            self.rect.y += self.vel
+
+        #jump
+
+        #handle the animation
+        self.counter += 1
+        flap_cooldown = 5
+
+        if self.counter > flap_cooldown:
+            self.counter = 0
+            self.index += 1
+            if self.index >= len(self.images):
+                self.index = 0
+        self.image = self.images[self.index]
 
 bird_group = pygame.sprite.Group()
 
@@ -43,6 +72,7 @@ while run:
     screen.blit(bg, (0,0))
 
     bird_group.draw(screen)
+    bird_group.update()
 
     #draw and scroll the ground
     screen.blit(ground_img, (ground_scroll, 768))
