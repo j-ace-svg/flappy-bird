1d7fe97e5605c2f205427b2c753478ba1a0727b7
John Montgomery <j.ace.svg@gmail.com>
2024-08-23 18:35:21 +0000

    Add shell of bird

--- main.py
+++ main.py
@@ -20,6 +20,20 @@ scroll_speed = 4
 bg = pygame.image.load('img/bg.png')
 ground_img = pygame.image.load('img/ground.png')
 
+
+class Bird(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('img/bird1.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+
+bird_group = pygame.sprite.Group()
+
+flappy = Bird(100, int(screen_height / 2))
+
+bird_group.add(flappy)
+
 run = True
 while run:
 
@@ -28,9 +42,13 @@ while run:
     #draw background
     screen.blit(bg, (0,0))
 
+    bird_group.draw(screen)
+
     #draw and scroll the ground
     screen.blit(ground_img, (ground_scroll, 768))
     ground_scroll -= scroll_speed
+    if abs(ground_scroll) > 35:
+        ground_scroll = 0
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
